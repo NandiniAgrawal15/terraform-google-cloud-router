7412173c12c1e038559543d436c4242126ccc3de
NandiniAgrawal15 <agrawalnandini@google.com>
2025-12-09 07:57:50 +0000

    asjknq

--- modules/interconnect_attachment/outputs.tf
+++ modules/interconnect_attachment/outputs.tf
@@ -19,6 +19,8 @@ output "attachment" {
   description = "The created attachment"
 }
 
+swqsqw
+
 output "customer_router_ip_address" {
   value       = google_compute_interconnect_attachment.attachment.customer_router_ip_address
   description = "IPv4 address + prefix length to be configured on the customer router subinterface for this interconnect attachment."
